e932109fa4317b936cc1b84cb2136a7519d9f53c
Vladislav Prozorov <93047063+vprozorov93@users.noreply.github.com>
2022-03-02 19:52:02 +0300

    Update test_pytest.py

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -3,17 +3,22 @@ from ya_api import YaUploader, token
 
 @pytest.fixture
 def data():
-    foo = YaUploader(token)
-    return foo, 'TestFolder13'
+    return 'TestFolder13'
 
 
 class TestFunction:
+    def setup(self):
+        self.YaUploader = YaUploader(token)
+        
     def test_create_folder(self, data):
-        assert data[0].create_folder(data[1]) == 201
-        assert data[0].get_folder_info(data[1]) == 200
+        yad = self.YaUploader
+        assert yad.create_folder(data) == 201
+        assert yad.get_folder_info(data) == 200
 
     def test_erorr_create_folder(self, data):
-        assert data[0].create_folder(data[1]) == 201
+        yad = self.YaUploader
+        assert yad.create_folder(data) == 201
 
     def test_error_check_folder(self, data):
-        assert data[0].get_folder_info(data[1]+'1') == 200
+        yad = self.YaUploader
+        assert yad.get_folder_info(data+'1') == 200
