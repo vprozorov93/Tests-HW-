db6a5afb1e67545d4fce2833b9369d4345897e15
Vladislav Prozorov <93047063+vprozorov93@users.noreply.github.com>
2022-03-21 09:03:30 +0300

    Update test_selenium.py

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-
+login = ''
+password = ''
 driver = webdriver.Chrome(executable_path=r"/Users/vladislavprozorov/Downloads/chromedriver")
 
 driver.get("https://passport.yandex.ru/auth/")
 print(driver.title)
 element = driver.find_element(by=By.ID, value='passp-field-login')
-element.send_keys('revator.ru@yandex.ru', Keys.ARROW_DOWN)
+element.send_keys(login, Keys.ARROW_DOWN)
 driver.find_element(by=By.ID, value='passp:sign-in').click()
 driver.implicitly_wait(2)
 element2 = driver.find_element(by=By.ID, value='passp-field-passwd')
-element2.send_keys('N22K2W9L93B', Keys.ARROW_DOWN)
+element2.send_keys(password, Keys.ARROW_DOWN)
 driver.find_element(by=By.ID, value='passp:sign-in').click()
 # driver.close()
 # driver.quit()
